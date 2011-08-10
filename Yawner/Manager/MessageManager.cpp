b67feb85acb973e9749723b7a90a2000628adf8a
Henrik Hedelund <henrik@improove.se>
2011-08-11 00:13:40 +0200

    added license texts to new files

--- Yawner/Manager/MessageManager.cpp
+++ Yawner/Manager/MessageManager.cpp
@@ -1,3 +1,31 @@
+/**
+* Yawner
+*
+* Copyright (C) 2011 Henrik Hedelund (henke.hedelund@gmail.com)
+*
+* This file is part of the Qt based Yammer Client "Yawner".
+*
+* Yawner is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Yawner is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Yawner. If not, see <http://www.gnu.org/licenses/>.
+*
+* @category Yawner/Manager
+* @package Yawner/Manager
+* @author Henrik Hedelund <henke.hedelund@gmail.com>
+* @copyright 2011 Henrik Hedelund (henke.hedelund@gmail.com)
+* @license http://www.gnu.org/licenses/gpl.html GNU GPL
+* @link https://github.com/henkelund/Yawner
+*/
+
 #include "MessageManager.h"
 #include "Yawner.h"
 
